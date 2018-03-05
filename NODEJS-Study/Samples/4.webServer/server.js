41f6ac83018fe8a0b93a6d3e4e62a4c778206410
Joanna Qian <honggzb@users.noreply.github.com>
2018-03-05 15:32:08 -0500

    Update server.js

--- NODEJS-Study/Samples/4.webServer/server.js
+++ NODEJS-Study/Samples/4.webServer/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 8002;
 const app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -9,7 +10,7 @@ app.set('view engine', 'hbs');
 // if template is html
 // app.set('view engine', 'html');
 // app.engine('html', require('hbs').__express); 
-//app.use(express.static(__dirname+'/public'));    //remove for Middleware in line 26
+app.use(express.static(__dirname+'/public'));    //remove for Middleware in line 26
 
 //Middleware
 app.use((req,res,next) => {  
@@ -24,9 +25,9 @@ app.use((req,res,next) => {
   next();
 });
 
-app.use((req,res,next) => {          //must remove app.use(express.static(__dirname+'/public'));
-  res.render('maintenance.hbs');
-})
+// app.use((req,res,next) => {          //must remove app.use(express.static(__dirname+'/public'));
+//   res.render('maintenance.hbs');
+// })
 
 //hbs helper
 hbs.registerHelper('getCurrentYear', ()=>{
@@ -51,10 +52,17 @@ app.get('/about',(req,res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res)=> {
   res.send({errorMessage: 'Unable to handle request'});
 });
 
 const server = app.listen(8002, "localhost", () => {
-  console.log("服务器已经在8002端口启动");
+  console.log(`服务器已经在${port}端口启动`);
 });
