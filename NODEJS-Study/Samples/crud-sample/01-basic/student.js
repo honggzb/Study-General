4eb03509a61e24eca268e8827a2ea2f7cb23ee08
Joanna Qian <honggzb@users.noreply.github.com>
2018-09-23 18:16:49 -0400

    Update student.js

--- NODEJS-Study/Samples/crud-sample/01-basic/student.js
+++ NODEJS-Study/Samples/crud-sample/01-basic/student.js
@@ -1,5 +1,8 @@
 /**
  * 数据操作模块， 只处理数据的CRUD
+ * 职责：操作文件中的数据，只处理数据，不关心业务
+ * 
+ * 封装异步 API
  */
 
  var fs = require('fs');
@@ -13,6 +16,11 @@ callback第二个参数是结果
   成功是 数组
   错误是 undefined
 */
+
+/**
+ * 获取学生列表
+ * @param  {Function} callback 回调函数
+ */
  exports.find = function(callback) {
     fs.readFile(dbPath, 'utf8',  (err, data) => {
         if(err){
@@ -22,6 +30,11 @@ callback第二个参数是结果
     });
 }
 
+/**
+ * 根据 id 获取学生信息对象
+ * @param  {Number}   id       学生 id
+ * @param  {Function} callback 回调函数
+ */
 exports.findById = function(id, callback) {
     fs.readFile(dbPath, 'utf8',  (err, data) => {
         if(err){
@@ -35,6 +48,11 @@ exports.findById = function(id, callback) {
     });
 }
 
+/**
+ * 添加保存学生
+ * @param  {Object}   student  学生对象
+ * @param  {Function} callback 回调函数
+ */
 exports.save = function(student, callback){
     fs.readFile(dbPath, 'utf8', function (err, data) {
         if(err){
@@ -43,6 +61,7 @@ exports.save = function(student, callback){
         var students = JSON.parse(data).students;
         student.id = students[students.length-1].id + 1;
         students.push(student);
+        // 把对象数据转换为字符串
         var fileData = JSON.stringify({ students: students });
         fs.writeFile(dbPath, fileData, (err) => {
             if(err){
@@ -53,12 +72,18 @@ exports.save = function(student, callback){
     }); 
 }
 
+/**
+ * 更新学生
+ */
 exports.updateById = function(student, callback){
     fs.readFile(dbPath, 'utf8', function (err, data) {
         if(err){
             return callback(err);
         }
         var students = JSON.parse(data).students;
+        // EcmaScript 6 中的一个数组方法：find
+        // 需要接收一个函数作为参数
+        // 当某个遍历项符合 item.id === student.id 条件的时候，find 会终止遍历，同时返回遍历项
         var stu = students.find( (item) => {    //es6, 当满足条件时，终止遍历并返回遍历项
             return item.id == student.id
         });
@@ -77,6 +102,9 @@ exports.updateById = function(student, callback){
     });
 }
 
+/**
+ * 删除学生
+ */
 exports.deleteById = function(id, callback){
     fs.readFile(dbPath, 'utf8', function (err, data) {
         if(err){
