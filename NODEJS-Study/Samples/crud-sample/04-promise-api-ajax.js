79686c97c9b1a2a25e7725891235a09677596ffa
Joanna Qian <honggzb@users.noreply.github.com>
2018-10-02 23:18:26 -0400

    Update 04-promise-api-ajax.js

--- NODEJS-Study/Samples/crud-sample/04-promise-api-ajax.js
+++ NODEJS-Study/Samples/crud-sample/04-promise-api-ajax.js
@@ -3,8 +3,8 @@ pGet('http://127.0.0.1:3000/users/4')
         console.log(data)
       })
 
-    function pGet(url, callback) {
-      return new Promise(function (resolve, reject) {
+function pGet(url, callback) {
+   return new Promise(function (resolve, reject) {
         var oReq = new XMLHttpRequest()
         // 当请求加载成功之后要调用指定的函数
         oReq.onload = function () {
@@ -17,17 +17,17 @@ pGet('http://127.0.0.1:3000/users/4')
         }
         oReq.open("get", url, true)
         oReq.send()
-      })
-    }
+  })
+}
     // 这个 get 是 callback 方式的 API
     // 可以使用 Promise 来解决这个问题
-    function get(url, callback) {
-      var oReq = new XMLHttpRequest()
+function get(url, callback) {
+  var oReq = new XMLHttpRequest()
       // 当请求加载成功之后要调用指定的函数
-      oReq.onload = function () {
+  oReq.onload = function () {
         // 我现在需要得到这里的 oReq.responseText
         callback(oReq.responseText)
-      }
-      oReq.open("get", url, true)
-      oReq.send()
-    }
+  }
+  oReq.open("get", url, true)
+  oReq.send()
+}
