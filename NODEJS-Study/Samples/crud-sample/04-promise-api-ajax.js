e1b0cb35bfebcaa755f7a43cb2db43aba63657e9
Joanna Qian <honggzb@users.noreply.github.com>
2018-10-02 23:42:41 -0400

    Update 04-promise-api-ajax.js

--- NODEJS-Study/Samples/crud-sample/04-promise-api-ajax.js
+++ NODEJS-Study/Samples/crud-sample/04-promise-api-ajax.js
@@ -1,15 +1,36 @@
+//1) jquery已支持Promise的异步调用
+var data = {};   //global variable
+$.get('http://127.0.0.1:3000/users/4')
+    .then(function (data1) {
+        data.user = data1;
+        return $.get('http://127.0.0.1:3000/jobs')
+    })
+    .then(function (data2) {
+        data.jobs = data2;
+        console.log(data);
+    })
+// 2) 自定义
+var data = {};   //global variable
 pGet('http://127.0.0.1:3000/users/4')
       .then(function (data) {
-        console.log(data)
+         data.user = data1;
+         return pGet('http://127.0.0.1:3000/jobs')
       })
-
+      .then(function (data) {
+        data.jobs = data2;
+        console.log(data);
+      })
+//也支持
+pGet('http://127.0.0.1:3000/users/4', function(data){
+      console.log(data);
+ })
 function pGet(url, callback) {
    return new Promise(function (resolve, reject) {
         var oReq = new XMLHttpRequest()
         // 当请求加载成功之后要调用指定的函数
         oReq.onload = function () {
-          // 我现在需要得到这里的 oReq.responseText
-          callback && callback(JSON.parse(oReq.responseText))
+          // 得到这里的 oReq.responseText
+          callback && callback(JSON.parse(oReq.responseText))   //也支持callback
           resolve(JSON.parse(oReq.responseText))
         }
         oReq.onerror = function (err) {
