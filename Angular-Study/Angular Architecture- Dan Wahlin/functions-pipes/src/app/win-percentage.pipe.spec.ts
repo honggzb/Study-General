158a414201e05bc8bd5392f50962008ed4a49111
joanna <honggzb@gmail.com>
2019-07-31 15:05:13 -0400

    angular architect fixed
    angular architect fixed

--- Angular-Study/Angular Architecture- Dan Wahlin/functions-pipes/src/app/win-percentage.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WinPercentagePipe } from './win-percentage.pipe';
-
-describe('WinPercentagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new WinPercentagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
