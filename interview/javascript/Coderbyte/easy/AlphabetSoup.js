e499f559e2105c24fc9506d9ad1b6b48c060dfde
Joanna Qian <honggzb@users.noreply.github.com>
2024-01-08 00:00:54 -0500

    Update AlphabetSoup.js

--- interview/javascript/Coderbyte/easy/AlphabetSoup.js
+++ interview/javascript/Coderbyte/easy/AlphabetSoup.js
@@ -1,8 +1,24 @@
-/*
-QUESTION:
-Have the function AlphabetSoup(str) take the str string parameter being passed and 
-return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
-*/
+/***************************************************************************************
+*                                                                                      *
+*                  CODERBYTE BEGINNER CHALLENGE                                        *
+*                                                                                      *
+*  Alphabet Soup                                                                       *
+*  Have the function AlphabetSoup(str) take the str string parameter being passed      *
+*  and return the string with the letters in alphabetical order                        *
+*  (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be       *
+*  included in the string.                                                             *
+*                                                                                      *
+*  SOLUTION                                                                            *
+*  The Array has a built-in sort function but String does not. The first step is to    *
+*  convert the string to an Array and sort it in alphabetical order. Then covert the   *
+*  Array back to a string with the join() function.                                    *
+*                                                                                      *
+*  Steps for solution                                                                  *
+*    1) Convert string to an array                                                     *
+*    2) Sort array in alphabetical order                                               *
+*    3) Convert array back to a string                                                 *
+*                                                                                      *
+***************************************************************************************/
 function AlphabetSoup(str) {
   return str.split("").sort().join("");
 }
