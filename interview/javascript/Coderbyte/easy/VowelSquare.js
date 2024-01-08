401f8658fb9768671fa4bc13a1f694ddfa37a64c
Joanna Qian <honggzb@users.noreply.github.com>
2024-01-08 00:43:09 -0500

    Update VowelSquare.js

--- interview/javascript/Coderbyte/easy/VowelSquare.js
+++ interview/javascript/Coderbyte/easy/VowelSquare.js
@@ -12,7 +12,7 @@ Input:["aqrst", "ukaei", "ffooo"]        --> "1-2"
 Input:["gg", "ff"]                       -->  "not found"
 */
 function isVowel(c){
-	switch(c){
+    switch(c){
 		case 'a': case 'A':
 		case 'e': case 'E':
 		case 'i': case 'I':
@@ -21,23 +21,64 @@ function isVowel(c){
 			return true;
 		default:
 			return false;
-	}
+}
 }
 
 function VowelSquare(strArr) {
-	var arr = [];
+    var arr = [];
     var i,j;
     //Make string array a 2D array.
     for(i=0;i<strArr.length;i++){
         arr.push(strArr[i].split(""));
     }
-	for(i=0;i<arr.length-1;i++){
+    for(i=0;i<arr.length-1;i++){
 		for(j=0;j<arr[i].length-1;j++){
 			if( isVowel(arr[i][j]) && isVowel(arr[i+1][j]) && isVowel(arr[i][j+1])  && isVowel(arr[i+1][j+1]) ){
 				return i+"-"+j;
 			}
 		}
-	}
-	return "not found";
+   }
+  return "not found";
+}
+
+
+/***************************************************************************************
+*                                                                                      *
+*                  CODERBYTE BEGINNER CHALLENGE                                        *
+*                                                                                      *
+*  Vowel Count                                                                         *
+*  Using the JavaScript language, have the function VowelCount(str) take the str       *
+*  string parameter being passed and return the number of vowels the string contains   *
+*  (ie. "All cows eat grass" would return 5). Do not count y as a vowel for this       *
+*  challenge.                                                                          *
+*                                                                                      *
+*  SOLUTION                                                                            *
+*  I will be using two for loops that will be nested. I will be searching thru all     *
+*  entries in my vowel array in the outter loop and searching through each letter      *
+*  in the string in the inner loop. I will be comparing each vowel to every letter     *
+*  in the string. If it matches then I will be incrememting by total by 1.
+*                                                                                      *
+*  Steps for solution                                                                  *
+*    1) create my vowel arrray.                                                        *
+*    2) Initialize tot to zero.                                                        *
+*    3) Loop through each vowel and compare to each letter in string                   *
+*    4) If match then increment tot by 1                                               *
+*    5) Return tot as answer                                                           *
+*                                                                                      *
+***************************************************************************************/
+function VowelCount(str) { 
+
+  var vowelArr = ["a","e","i","o","u"];
+  var tot = 0;
+
+  for (var i = 0; i < vowelArr.length; i++) {
+    for (var j = 0; j < str.length; j++) {
+        if (str[j] === vowelArr[i]) {
+            tot++;
+        }
+    }
+  }
+  return tot;
+         
 }
 
