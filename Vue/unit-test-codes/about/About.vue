2e08b593de2af630dd74160e09428edf1fe6ffbb
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2024-06-19 11:55:08 -0400

    Create About.vue

---
+++ Vue/unit-test-codes/about/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <h2>Count: {{ count }}</h2>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'About',
+  components: {},
+  data() {
+    return { count: 0 }
+  },
+  methods: {
+    increment() { this.count += 1 },
+  },
+})
+</script>
