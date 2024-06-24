f62a5f3b80db8f312685565f12b14a428c9cb050
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2024-06-24 12:40:03 -0400

    Update request.ts

--- Project-document/edu-app-vue3/src/utils/request.ts
+++ Project-document/edu-app-vue3/src/utils/request.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useUserStore } from '@/stores/user'
+import { ElMessage } from 'element-plus';
 //1. 创建axios对象
 const service = axios.create();
 //2. 请求拦截器
@@ -18,7 +19,29 @@ service.interceptors.request.use(config => {
 service.interceptors.response.use(response => {
   //判断code码
   return response.data;
-},error => {
+}, error => {
+  let message = '';
+  let status = error.response.status;
+  switch(status) {
+    case 401:
+      message = 'Token过期'
+      break
+    case 403:
+       message = '无权访问'
+      break
+    case 404:
+      message = '请求地址错误'
+      break
+    case 500:
+      message = '服务器出现问题'
+      break
+    default:
+      message = '网络出现问题'
+  }
+  ElMessage({
+    type: 'error',
+    message
+  })
   return Promise.reject(error);
 });
 
