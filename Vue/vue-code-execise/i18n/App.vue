c8b3ad1ac0acc52147272598f6096a046bd6c3b5
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2024-07-02 11:41:18 -0400

    Create App.vue

---
+++ Vue/vue-code-execise/i18n/App.vue
@@ -0,0 +1,47 @@
+<script>
+import { watch } from "vue";
+import { useI18n } from "vue-i18n";
+export default {
+  name: "App",
+  setup() {
+    const { t, locale } = useI18n();
+
+    watch(locale, (newlocale) => {
+      localStorage.setItem("locale", newlocale);
+    });
+
+    return {
+      t,
+      locale,
+    };
+  },
+};
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <nav>
+    <select v-model="locale">
+      <option>zh-TW</option>
+      <option>en-US</option>
+      <option>ja-JP</option>
+    </select>
+  </nav>
+  <p>{{ t("cancel") }}</p>
+  <p>{{ t("email") }}</p>
+  <p>{{ t("date") }}</p>
+  <p>{{ t("subject") }}</p>
+  <p>{{ t("message") }}</p>
+  <p>{{ t("upload") }}</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
