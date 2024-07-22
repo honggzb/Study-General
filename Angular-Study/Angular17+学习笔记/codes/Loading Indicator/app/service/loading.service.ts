e07f1e98cbb32ebf5897ac071581460d53e68e6e
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2024-07-22 16:06:23 -0400

    Create loading.service.ts

---
+++ Angular-Study/Angular17+学习笔记/codes/Loading Indicator/app/service/loading.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
+
+  loadingOn() {
+    this.loadingSubject.next(true);
+  }
+
+  loadingOff() {
+    this.loadingSubject.next(false);
+  }
+}
