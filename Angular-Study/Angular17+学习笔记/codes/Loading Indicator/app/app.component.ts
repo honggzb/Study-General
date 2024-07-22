8e0b4087ba862fb9376ac766d637956dba89276b
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2024-07-22 16:10:42 -0400

    Create app.component.ts

---
+++ Angular-Study/Angular17+学习笔记/codes/Loading Indicator/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoadingIndicatorComponent } from './components/loading/loading.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LoadingIndicatorComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angula-loading';
+}
