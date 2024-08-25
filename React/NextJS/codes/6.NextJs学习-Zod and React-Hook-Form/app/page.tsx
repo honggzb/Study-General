b60717a1df6b79472b7e85f662b9f3e894489e27
honggzb <honggzb@gmail.com>
2024-08-25 02:36:42 -0400

    add zod codes
    add zod codes

---
+++ React/NextJS/codes/6.NextJs学习-Zod and React-Hook-Form/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Form from "./components/Form";
+
+function Home() {
+ 
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <Form />
+    </main>
+  );
+}
+
+export default Home;
