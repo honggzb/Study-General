b9e5ff4ba588c7cc620bf2d7964698efbdfb1918
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2025-12-02 23:29:33 -0500

    Update page.tsx

--- React/sample-codes/toggle-theme/01-next-themes+tailwind/app/toggle-test/page.tsx
+++ React/sample-codes/toggle-theme/01-next-themes+tailwind/app/toggle-test/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const ToggleThemeTestPage = () => {
   return (
-    <div className="m-10 rounded-lg bg-white dark:bg-black px-6 py-8 shadow-xl ring-1 ring-slate-900/5">
-      <h3 className='text-base font-medium tracking-tight text-slate-800 dark:text-white'>Write Upside-Down</h3>
+    <div className="m-10 rounded-lg px-6 py-8 shadow-lg ring-1 ring-slate-900/5 dark:ring-slate-900 dark:shadow-gray-700">
+      <h3 className='text-base font-medium tracking-tight text-slate-800'>Write Upside-Down</h3>
       <p className='mt-2 text-sm text-slate-500 dark:text-blue-100'>Pariatur incididunt culpa laboris ea ut. Deserunt nostrud sit qui non consequat amet. Elit magna exercitation quis do mollit laborum. Laboris ad magna anim sunt nisi mollit irure dolore consectetur laborum. Culpa ad velit sint duis sit Lorem duis incididunt. Tempor nisi id fugiat ullamco mollit. Esse ea exercitation qui cupidatat eu tempor tempor minim exercitation aliquip consectetur.<br /></p>
       <ToggleThemePage />
     </div>
