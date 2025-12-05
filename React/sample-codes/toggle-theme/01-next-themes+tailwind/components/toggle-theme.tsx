b2691fce0c7d9989c7376c617b6d25052ba4ad6f
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2025-12-05 01:36:21 -0500

    Update toggle-theme.tsx

--- React/sample-codes/toggle-theme/01-next-themes+tailwind/components/toggle-theme.tsx
+++ React/sample-codes/toggle-theme/01-next-themes+tailwind/components/toggle-theme.tsx
@@ -17,6 +17,11 @@ const ToggleThemePage = () => {
           <p>Theme is {theme}</p>
            <Moon />
         </button>
+        {/* 动画效果来自uiverse.io */}
+        {/* <label className="relative inline-flex items-center cursor-pointer">
+        <input className="sr-only peer" defaultValue type="checkbox" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}/>
+        <div className="w-24 h-12 rounded-full ring-0 peer duration-500 outline-none bg-gray-200 overflow-hidden before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center before:content-['☀️'] before:absolute before:h-10 before:w-10 before:top-1/2 before:bg-white before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-700 peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-[#383838] after:content-['🌑'] after:absolute after:bg-[#1d1d1d] after:rounded-full after:top-[4px] after:right-1 after:translate-y-full after:w-10 after:h-10 after:opacity-0 after:transition-all after:duration-700 peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:translate-y-0" />
+      </label> */}
     </>
   )
 }
