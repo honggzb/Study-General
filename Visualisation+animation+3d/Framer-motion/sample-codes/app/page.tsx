87b411ad0b1b8dd8a08c2434de8818ff6cf19314
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2026-02-02 13:46:49 -0500

    Update page.tsx

--- Visualisation+animation+3d/Framer-motion/sample-codes/app/page.tsx
+++ Visualisation+animation+3d/Framer-motion/sample-codes/app/page.tsx
@@ -1,27 +1,27 @@
+import ChangeTextButton from '@/components/6ChangeTextButton';
+import TextAnimations from '@/components/7TextAnimations';
 import AnimateButton from '@/components/1AnimateButton';
 import CardContent from '@/components/2Card';
 import Dashboard from '@/components/3Dashboard';
 import MotionHooks from '@/components/4MotionHooks';
+import Timeline from '@/components/4Timeline';
 import Layout from '@/components/5Layout';
 import Navbar from '@/components/5Navbar';
-import ChangeText from '@/components/6ChangeText';
-import ChangeTextButton from '@/components/6ChangeTextButton';
 import ChangeNumber from '@/components/6ChangeNumber';
-import Timeline from '@/components/4Timeline';
 
 export default function Home() {
   return (
     <>
-      {/* <AnimateButton /> */}
-      {/* <CardContent /> */}
-      {/* <Dashboard /> */}
-      {/* <MotionHooks /> */}
-      {/* <Layout /> */}
-      {/* <Navbar /> */}
-      {/* <ChangeText /> */}
+      <AnimateButton />
+      <CardContent />
+      <Dashboard />
+      <MotionHooks />
+      <Timeline />
+      <Layout />
+      <Navbar />
+      <ChangeNumber />
       <ChangeTextButton />
-      {/* <ChangeNumber /> */}
-      {/* <Timeline /> */}
+      <TextAnimations />
     </>
   );
 }
