88015bece7df8a93aece0243cfb65b2a014ce5f0
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2026-02-04 12:37:20 -0500

    Create 7CirclePhoto-animate.tsx

---
+++ Visualisation+animation+3d/Framer-motion/sample-codes/components/7CirclePhoto-animate.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { motion } from "motion/react";
+
+const CirclePhoto = () => {
+  return (
+    <div className="w-full h-full relative mt-50">
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 2, duration: 0.4, ease: "easeIn" }
+            }}
+        >
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" }
+                }}
+                className="absolute w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+            >
+                 <img src="./assets/photo.png" quality={100} alt="" className="object-contain" />
+            </motion.div>
+            {/* circle */}
+            <motion.svg
+                className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+                fill="transparent"
+                viewBox="0 0 506 506"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <motion.circle
+                    cx="253" cy="253" r="250"
+                    stroke="#00ff99"
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    initial={{ strokeDasharray: "24 10 0 0" }}
+                    animate={{
+                        strokeDasharray: ["15 120 25 25" ,"16 25 92 72", "4 250 22 22"],
+                        rotate: [120, 360],
+                    }}
+                    transition={{ duration: 20, repeat: Infinity, repeatType: "reverse" }}
+                />
+            </motion.svg>
+        </motion.div>
+    </div>
+  )
+}
+
+export default CirclePhoto
